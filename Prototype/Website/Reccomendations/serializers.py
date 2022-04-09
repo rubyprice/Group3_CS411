56dccfd5a7dae4f994cb93e4b1eab9a10f6b8a82
APXBUCS <98182047+APXBUCS@users.noreply.github.com>
2022-04-08 23:28:50 -0400

    Delete Prototype directory

--- Prototype/Website/Reccomendations/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import UserData
-
-class UserDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UserData
-        fields = ('id','userName', 'zipcode', 'weather', 'date')
--- Prototype/Website/Reccomendations/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from Reccomendations import views
-
-
-urlpatterns = [
-    re_path(r'^', views.UserDataAPI)
-] 
